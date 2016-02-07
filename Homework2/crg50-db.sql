370cab07b2ed9b546c46e38d857a445b4272b7a0
Chris Grant <cgrant1995@me.com>
2016-02-07 14:05:01 -0500

    Sunday, February 7, 2016 at 2:05:01 PM

--- Homework2/crg50-db.sql
+++ Homework2/crg50-db.sql
@@ -1,6 +1,25 @@
 --Chris Grant, crg50@pitt.edu
 DROP TABLE Conversation CASCADE CONSTRAINTS;
+DROP TABLE Contacts CASCADE CONSTRAINTS ;
+DROP TABLE Messages CASCADE CONSTRAINTS ;
+DROP TABLE Recipients CASCADE CONSTRAINTS ;
 --#1
+
+CREATE TABLE Contacts (
+  user_ID number(10) NOT NULL,
+  fname varchar2(32),
+  lname varchar2(32),
+  cell varchar2(16),
+  city varchar2(32),
+  country varchar2(32),
+  PRIMARY KEY (user_ID)
+);
+
+CREATE TABLE Conversation (
+  convID number(10) NOT NULL,
+  duration number(10),
+  PRIMARY KEY (convID)
+);
 CREATE TABLE Messages (
   msgID  NUMBER(10) PRIMARY KEY,
   sender_ID NUMBER(10) NOT NULL,
@@ -19,20 +38,4 @@ CREATE TABLE Recipients (
   PRIMARY KEY (msgID,user_ID),
   FOREIGN KEY (msgID) REFERENCES Messages (msgID),
   FOREIGN KEY (user_ID) REFERENCES Contacts (user_ID)
-);
-
-CREATE TABLE Contacts (
-  user_ID number(10) NOT NULL,
-  fname varchar2(32),
-  lname varchar2(32),
-  cell varchar2(16),
-  city varchar2(32),
-  country varchar2(32),
-  PRIMARY KEY (user_ID)
-);
-
-CREATE TABLE Conversation (
-  convID number(10) NOT NULL,
-  duration number(10),
-  PRIMARY KEY (convID)
 );
