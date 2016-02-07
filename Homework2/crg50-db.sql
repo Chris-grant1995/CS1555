bde3a78b78bee43c7f829b67cd6629845142935c
Chris Grant <cgrant1995@me.com>
2016-02-07 13:40:26 -0500

    Sunday, February 7, 2016 at 1:40:26 PM

--- Homework2/crg50-db.sql
+++ Homework2/crg50-db.sql
@@ -3,39 +3,39 @@
 --#1
 CREATE TABLE Messages
 (
-  msgID: NUMBER PRIMARY KEY,
-  sender_ID: NUMBER NOT NULL,
+  msgID  NUMBER(10) PRIMARY KEY,
+  sender_ID NUMBER(10) NOT NULL,
   FOREIGN KEY (sender_ID) REFERENCES Contacts(user_ID),
-  time_sent: TIMESTAMP NOT NULL ,
-  time_read: TIMESTAMP,
-  convID: NUMBER NOT NULL ,
+  time_sent TIMESTAMP NOT NULL ,
+  time_read TIMESTAMP,
+  convID NUMBER(10) NOT NULL ,
   FOREIGN KEY (convID) REFERENCES Conversation(convID),
-  msg_text: VARCHAR2,
-  spam: NUMBER
+  msg_text VARCHAR2(1024),
+  spam NUMBER(1)
 
 );
 
 CREATE TABLE Recipients (
-  msgID: NUMBER,
-  user_ID: number,
+  msgID  NUMBER(10),
+  user_ID  number(10),
   PRIMARY KEY (msgID,user_ID),
   FOREIGN KEY (msgID) REFERENCES Messages(msgID),
   FOREIGN KEY (user_ID) REFERENCES Contacts(user_ID)
 );
 
 CREATE TABLE Contacts (
-  user_ID: number NOT NULL,
-  fname: varchar2,
-  lname: varchar2,
-  cell: varchar2,
-  city: varchar2,
-  country: varchar2,
+  user_ID number(10) NOT NULL,
+  fname varchar2(32),
+  lname varchar2(32),
+  cell varchar2(16),
+  city varchar2(32),
+  country varchar2(32),
   PRIMARY KEY (user_ID)
 
 );
 
 CREATE TABLE Conversation (
-  convID: number NOT NULL,
-  duration: number,
+  convID number(10) NOT NULL,
+  duration number(1024),
   PRIMARY KEY (convID)
 );
