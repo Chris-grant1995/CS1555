63a651a157caa2949fa8785feedef8b7cf5e976c
Chris Grant <cgrant1995@me.com>
2016-02-11 17:52:06 -0500

    Thursday, February 11, 2016 at 5:52:06 PM

--- Homework2/crg50-db.sql
+++ Homework2/crg50-db.sql
@@ -202,4 +202,7 @@ INSERT INTO Recipients VALUES (
 );
 
 
-
+SELECT fname,lname
+FROM Contacts JOIN Messages ON Contacts.user_ID = Messages.sender_ID JOIN Recipients ON Messages.msgID = Recipients.msgID
+WHERE Recipients.user_ID = (select user_ID from Contacts where fname = 'Francis' AND  lname = 'Lefebvre')
+GROUP BY fname,lname;
--- Homework2/crg50-query.sql
+++ Homework2/crg50-query.sql
@@ -3,4 +3,10 @@
 --A
 SELECT user_ID
   FROM Contacts
-WHERE cell = '412-624-8445'
+WHERE cell = '412-624-8445';
+
+--B
+SELECT fname,lname
+FROM Contacts JOIN Messages ON Contacts.user_ID = Messages.sender_ID JOIN Recipients ON Messages.msgID = Recipients.msgID
+WHERE Recipients.user_ID = (select user_ID from Contacts where fname = 'Francis' AND  lname = 'Lefebvre')
+GROUP BY fname,lname;
