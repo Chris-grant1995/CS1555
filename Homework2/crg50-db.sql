5ae2bb8d4a35eabeeb9a512ba104042304f6f27f
Chris Grant <cgrant1995@me.com>
2016-02-11 17:25:29 -0500

    Thursday, February 11, 2016 at 5:25:29 PM

--- Homework2/crg50-db.sql
+++ Homework2/crg50-db.sql
@@ -1,50 +1,50 @@
 --Chris Grant, crg50@pitt.edu
 DROP TABLE Conversation CASCADE CONSTRAINTS;
-DROP TABLE Contacts CASCADE CONSTRAINTS ;
-DROP TABLE Messages CASCADE CONSTRAINTS ;
-DROP TABLE Recipients CASCADE CONSTRAINTS ;
+DROP TABLE Contacts CASCADE CONSTRAINTS;
+DROP TABLE Messages CASCADE CONSTRAINTS;
+DROP TABLE Recipients CASCADE CONSTRAINTS;
 --#1
 
 CREATE TABLE Contacts (
-  user_ID number(10) NOT NULL,
-  fname varchar2(32),
-  lname varchar2(32),
-  cell varchar2(16),
-  city varchar2(32),
-  country varchar2(32),
+  user_ID NUMBER(10) NOT NULL,
+  fname   VARCHAR2(32),
+  lname   VARCHAR2(32),
+  cell    VARCHAR2(16),
+  city    VARCHAR2(32),
+  country VARCHAR2(32),
   PRIMARY KEY (user_ID)
 );
 
-CREATE TABLE Conversation (
-  convID number(10) NOT NULL,
-  duration number(10),
+CREATE TABLE Conversations (
+  convID   NUMBER(10) NOT NULL,
+  duration NUMBER(10),
   PRIMARY KEY (convID)
 );
 CREATE TABLE Messages (
-  msgID  NUMBER(10),
+  msgID     NUMBER(10),
   sender_ID NUMBER(10) NOT NULL,
   FOREIGN KEY (sender_ID) REFERENCES Contacts (user_ID),
-  time_sent TIMESTAMP NOT NULL ,
+  time_sent TIMESTAMP  NOT NULL,
   time_read TIMESTAMP,
-  convID NUMBER(10) NOT NULL ,
+  convID    NUMBER(10) NOT NULL,
   FOREIGN KEY (convID) REFERENCES Conversation (convID),
-  msg_text VARCHAR2(1024),
-  spam NUMBER(1),
-  PRIMARY KEY(msgID)
+  msg_text  VARCHAR2(1024),
+  spam      NUMBER(1),
+  PRIMARY KEY (msgID)
 );
 
 CREATE TABLE Recipients (
-  msgID  NUMBER(10),
-  user_ID  NUMBER(10),
-  PRIMARY KEY (msgID,user_ID),
+  msgID   NUMBER(10),
+  user_ID NUMBER(10),
+  PRIMARY KEY (msgID, user_ID),
   FOREIGN KEY (msgID) REFERENCES Messages (msgID),
   FOREIGN KEY (user_ID) REFERENCES Contacts (user_ID)
 );
 
 --#2
 --A
-ALTER TABLE Messages DROP COLUMN  time_read;
-ALTER TABLE Recipients ADD  time_read TIMESTAMP;
+ALTER TABLE Messages DROP COLUMN time_read;
+ALTER TABLE Recipients ADD time_read TIMESTAMP;
 --B
 ALTER TABLE Conversation DROP COLUMN duration;
 --C
@@ -53,7 +53,7 @@ ALTER TABLE Messages ADD length NUMBER(1) DEFAULT 0;
 ALTER TABLE Messages ADD CHECK (spam = 0 OR spam = 1);
 --E
 ALTER TABLE Conversation ADD userID NUMBER(10);
-ALTER TABLE Conversation ADD FOREIGN KEY (userID) REFERENCES Contacts(user_ID);
+ALTER TABLE Conversation ADD FOREIGN KEY (userID) REFERENCES Contacts (user_ID);
 --F
 ALTER TABLE Messages MODIFY spam DEFAULT 0;
 
@@ -107,13 +107,13 @@ INSERT INTO Contacts VALUES (
   'US'
 );
 INSERT INTO Conversation VALUES (
-    1,1
+  1, 1
 );
 INSERT INTO Conversation VALUES (
-    2,2
+  2, 2
 );
 INSERT INTO Conversation VALUES (
-    3,5
+  3, 5
 );
 
 INSERT INTO Messages VALUES (
