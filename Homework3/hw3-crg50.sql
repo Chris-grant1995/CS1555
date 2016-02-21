64719d26c86767b394807bf8df1589a7708d183d
Chris Grant <cgrant1995@me.com>
2016-02-21 12:01:56 -0500

    Sunday, February 21, 2016 at 12:01:56 PM

--- Homework3/hw3-crg50.sql
+++ Homework3/hw3-crg50.sql
@@ -11,20 +11,17 @@ ORDER BY Messages.msgID ASC ;
 --I have no idea what the hell this is asking
 
 --1C
-/*
-SELECT user_ID, count(time_read)
-FROM Recipients JOIN Messages on Recipients.msgID = Messages.msgID
-GROUP BY user_ID; -- Gets user ID and how many messages they have read
 
-SELECT DISTINCT EXTRACT(YEAR FROM Recipients.time_read)
-from Recipients; -- Gets unique years
+SELECT Year,Max(user_ID), MAX(NumRead)
+FROM (
+  SELECT user_ID, count(time_read) as NumRead, EXTRACT(YEAR FROM Recipients.time_read) AS Year
+  FROM Recipients JOIN Messages on Recipients.msgID = Messages.msgID
+  GROUP BY user_ID, EXTRACT(YEAR FROM Recipients.time_read)
+)
+GROUP BY Year;
 
-SELECT user_ID, count(time_read) as NumRead, EXTRACT(YEAR FROM Recipients.time_read) AS Year
-FROM Recipients JOIN Messages on Recipients.msgID = Messages.msgID
-GROUP BY user_ID, EXTRACT(YEAR FROM Recipients.time_read) ;
---Shows how many messages a given user read in a year
-SELECT max(NumRead), user_ID, Year
-FROM (SELECT user_ID, count(time_read) as NumRead, EXTRACT(YEAR FROM Recipients.time_read) AS Year FROM Recipients JOIN Messages on Recipients.msgID = Messages.msgID GROUP BY user_ID, EXTRACT(YEAR FROM Recipients.time_read))
-GROUP BY Year, user_ID;
-*/
-SELECT DISTINCT Year,user_ID, MAX(NumRead)  FROM (SELECT user_ID, count(time_read) as NumRead, EXTRACT(YEAR FROM Recipients.time_read) AS Year FROM Recipients JOIN Messages on Recipients.msgID = Messages.msgID GROUP BY user_ID, EXTRACT(YEAR FROM Recipients.time_read)) GROUP BY Year, MAX(user_ID);
+--2A
+CREATE VIEW ConversationLog
+    AS SELECT convID,msgID
+    FROM Messages
+    ORDER BY convID ASC ;
