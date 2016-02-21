39617de864fca17f1335817f11d7e406213e5e4b
Chris Grant <cgrant1995@me.com>
2016-02-21 11:04:14 -0500

    Sunday, February 21, 2016 at 11:04:14 AM

--- Homework3/hw3-crg50.sql
+++ Homework3/hw3-crg50.sql
@@ -4,4 +4,4 @@
 --1A
 SELECT * FROM Messages JOIN Recipients ON Messages.msgID = Recipients.msgID
 WHERE ((Messages.spam = 0) AND Recipients.time_read IS NOT NULL )
-GROUP BY Messages.msgID;
+ORDER BY Messages.msgID ASC ;
